4c462daf833adb623244abe2a38e8ac37f316fb0
Martins Aloba <martins.aloba@gmail.com>
2021-05-18 23:29:57 +0100

    refactor authentication process

--- server/db/db.js
+++ server/db/db.js
@@ -1,4 +1,7 @@
+const dotenv = require("dotenv");
 const Sequelize = require("sequelize");
+
+dotenv.config();
 
 const db = new Sequelize(process.env.DATABASE_URL || "postgres://localhost:5432/messenger", {
   logging: false
